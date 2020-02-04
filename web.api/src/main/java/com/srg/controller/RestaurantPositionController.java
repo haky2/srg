b9b747a7a613e20f1d49d740672e12270838c6ec
김연경 <kyeonk@godo.co.kr>
2020-02-04 14:51:19 +0900

    식당 위치 관련 파일 커밋 controller

--- web.api/src/main/java/com/srg/controller/RestaurantPositionController.java
+++ web.api/src/main/java/com/srg/controller/RestaurantPositionController.java
@@ -1,4 +1,21 @@
 package com.srg.controller;
 
+import com.srg.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurantPosition")
 public class RestaurantPositionController {
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addRestaurantPosition() {
+        return "add Complete";
+    }
+
 }
